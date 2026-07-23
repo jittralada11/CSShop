6e5667461a127e3fe31794d8028a58091ea0a290
jittralada11 <66011212012@msu.ac.th>
2026-07-23 20:53:26 +0700

    Update API_CONFIG baseUrl to point to the correct backend server

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,4 +1,4 @@
 // src/app/config/api.config.ts
 export const API_CONFIG = {
-  baseUrl: 'https://csgamebackend-production.up.railway.app'  // Original backend server with database connection
+  baseUrl: 'https://csshop-aksc.onrender.com/'  // Original backend server with database connection
 };
